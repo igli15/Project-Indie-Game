a8379e6c3bce380afd05e9f45339332a7559e2d2
Igli milaqi <milaqiigli@gmail.com>
2018-11-19 21:24:49 +0100

    Added player and player controller script which moves the player wit "wasd" keys.

---
+++ Project Indie Game/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour
+{
+
+	[SerializeField]
+	private float m_moveSpeed = 5;
+
+	private PlayerController m_playerController;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		m_playerController = GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 movementInput = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+		Vector3 velocity = movementInput * m_moveSpeed;
+		m_playerController.SetVelocity(velocity);
+	}
+
+}
---
+++ Project Indie Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+
+	private Vector3 m_velocity;
+	private Rigidbody m_rb;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		m_rb = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate()
+	{
+		m_rb.MovePosition(transform.position + m_velocity * Time.fixedDeltaTime);
+	}
+
+	public void SetVelocity(Vector3 velocity)
+	{
+		m_velocity = velocity;
+	}
+}
