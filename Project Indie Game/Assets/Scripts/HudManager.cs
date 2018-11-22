f1c08c40f5c438cf3ec0b578189fd2569d569080
Igli milaqi <milaqiigli@gmail.com>
2018-11-23 00:14:59 +0100

    Added visual feedback for scrolling as hud elements.

--- Project Indie Game/Assets/Scripts/HudManager.cs
+++ Project Indie Game/Assets/Scripts/HudManager.cs
@@ -1,16 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
-public class HudManager : MonoBehaviour {
+public class HudManager : MonoBehaviour
+{
+	
+	[Serializable]
+	public class HudIcon
+	{
+		public ACompanion companion;
+		public GameObject icon;
+	}
+	
+	[SerializeField]
+	private List<HudIcon> m_hudIcons = new List<HudIcon>();
 
+	[SerializeField]
+	[Range(1,2)]
+	private float m_scaleFactor = 1.2f;
+
+	[SerializeField] 
+	private float m_scaleTime = 0.2f;
+	
+	private Dictionary<ACompanion, GameObject> m_hudIconDictionary = new Dictionary<ACompanion, GameObject>();
+	
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
+		foreach (HudIcon icon in m_hudIcons)
+		{
+			m_hudIconDictionary.Add(icon.companion,icon.icon);
+
+			icon.companion.OnSelected += SelectIcon;
+			icon.companion.OnDeSelected += DeselectIcon;
+		}
+
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
+	}
+
+	public void SelectIcon(ACompanion companion)
+	{
+		m_hudIconDictionary[companion].transform.DOScale(m_scaleFactor,m_scaleTime);
+	}
+	public void DeselectIcon(ACompanion companion)
+	{
+		m_hudIconDictionary[companion].transform.DOScale(1, m_scaleTime);
 	}
 }
