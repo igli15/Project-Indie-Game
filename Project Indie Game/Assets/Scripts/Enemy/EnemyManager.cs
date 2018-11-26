fa46ad9797d580c9d391e58a3fe1c471498f190e
August <patrickpanda7@gmail.com>
2018-11-26 11:42:29 +0100

    Added custom editor

---
+++ Project Indie Game/Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+    //IMPORTANT
+    [System.Serializable]
+    public class EnemiesData
+    {
+        public int numberOfGoombas=0;
+        public int numberOfTurrets=0;
+    }
+    //IMPORTANT
+    [HideInInspector]
+    public int numWave = 0;
+
+    //IMPORTANT
+    public List<EnemySpawner> spawners;
+
+    //IMPORTANT
+    [HideInInspector]
+    public static EnemyManager instance;
+
+    //IMPORTANT
+    private EnemiesData[,] m_enemyWavesData;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    void Start () {
+        m_enemyWavesData = new EnemiesData[spawners.Count, numWave];
+
+    }
+	
+
+	void Update () {
+		
+	}
+}
