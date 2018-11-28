67ed18520bb83fe1bf9a68c89de9aab2250e36dc
Igli milaqi <milaqiigli@gmail.com>
2018-11-28 15:54:19 +0100

    Now the enemy are pulled toward the center of the "black hole"

---
+++ Project Indie Game/Assets/Scripts/Companions/BlackHole.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole : MonoBehaviour
+{
+	private float m_pullForce ;
+
+	private Dictionary<Transform, Vector3> m_enemiesInRangeDictionary ;
+	// Use this for initialization
+	void Start () 
+	{
+		m_enemiesInRangeDictionary = new Dictionary<Transform, Vector3>();
+	}
+	
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.transform.CompareTag("Enemy"))
+		{
+			m_enemiesInRangeDictionary.Add(other.transform,(transform.position - other.transform.position).normalized);
+		}
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.transform.CompareTag("Enemy"))
+		{
+			if (Vector3.Distance(transform.position, other.transform.position) <= 0.9f)
+			{
+				other.GetComponent<Rigidbody>().velocity = Vector3.zero;
+			}
+			else
+			{
+				other.GetComponent<Rigidbody>().velocity = (m_enemiesInRangeDictionary[other.transform] * m_pullForce);
+			}
+		}
+	}
+
+	public float PullForce
+	{
+		get { return m_pullForce; }
+		set { m_pullForce = value; }
+	}
+}
---
+++ Project Indie Game/Assets/Scripts/Companions/BlackHoleCompanion.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleCompanion : Companion
+{
+	[SerializeField] 
+	private GameObject m_blackHole;
+
+
+	[SerializeField] 
+	[Range(0.1f,2)]
+	private float m_pullForce = 1;
+	
+	private bool m_instantiatedBlackHole;
+	
+	private void Awake()
+	{
+		base.Awake();
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		base.Update();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.transform.CompareTag("Obstacle") || other.transform.CompareTag("Enemy"))
+		{
+			if(m_isThrown)
+			Activate();
+		}
+	}
+
+	public override void RangeReached()
+	{
+		base.RangeReached();
+		
+		if(m_isThrown)
+		Activate();
+	}
+
+	private void Reset()
+	{
+		base.Reset();
+		m_instantiatedBlackHole = false;
+	}
+
+	public override void Activate(GameObject other = null)
+	{
+		base.Activate(other);
+
+		if (m_instantiatedBlackHole == false)
+		{
+			GameObject blackHole = Instantiate(m_blackHole);
+			blackHole.transform.position = transform.position;
+			blackHole.transform.rotation = Quaternion.identity;
+			blackHole.GetComponent<BlackHole>().PullForce = m_pullForce;
+			m_instantiatedBlackHole = true;
+		}
+
+	}
+}
