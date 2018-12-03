ab3520695c1ede2d9c230bc42ec11efacc0882f1
Igli milaqi <milaqiigli@gmail.com>
2018-12-03 22:01:02 +0100

    Adde animation for the black hole companion.

---
+++ Project Indie Game/Assets/Scripts/Companions/CompanionAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompanionAnimation : MonoBehaviour
+{
+	private Animator m_animator;
+
+	private CompanionSteering m_steering;
+
+	private ACompanion m_companion;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		m_steering = GetComponent<CompanionSteering>();
+		m_animator = GetComponentInChildren<Animator>();
+		m_companion = GetComponent<ACompanion>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		m_animator.SetFloat("velocity",m_steering.NavMeshAgent.velocity.magnitude);
+		m_animator.SetBool("isThrown",m_companion.IsThrown);
+	}
+}
